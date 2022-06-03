2d0a42b28fcca31668aa77551191f2e1c9dd18a9
Aleksandr F <a7214370@gmail.com>
2022-06-03 17:55:22 +0300

    Initial Commit

---
+++ AwardsCollectionAppSwiftUI/AwardsCollectionAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionAppSwiftUIApp.swift
+//  AwardsCollectionAppSwiftUI
+//
+//  Created by Aleksandr F. on 03.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
