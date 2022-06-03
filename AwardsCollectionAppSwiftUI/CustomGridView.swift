c9f3e65d7d596b67cebf1ae2716bc2611bf456a9
Aleksandr F <a7214370@gmail.com>
2022-06-03 18:46:30 +0300

    Setup CustomGridView

---
+++ AwardsCollectionAppSwiftUI/CustomGridView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomGridView.swift
+//  AwardsCollectionAppSwiftUI
+//
+//  Created by Aleksandr F. on 03.06.2022.
+//
+
+import SwiftUI
+
+
+struct CustomGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomGridView()
+    }
+}
