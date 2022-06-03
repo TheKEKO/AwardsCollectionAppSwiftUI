87d134a0241b2f61ce3cf8d7b5a042003a005cf7
Aleksandr F <a7214370@gmail.com>
2022-06-03 18:46:13 +0300

    Setup AwardsView

---
+++ AwardsCollectionAppSwiftUI/AwardsView.swift
@@ -0,0 +1,21 @@
+//
+//  AwardsView.swift
+//  AwardsCollectionAppSwiftUI
+//
+//  Created by Aleksandr F. on 03.06.2022.
+//
+
+import SwiftUI
+
+
+struct AwardsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AwardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwardsView()
+    }
+}
