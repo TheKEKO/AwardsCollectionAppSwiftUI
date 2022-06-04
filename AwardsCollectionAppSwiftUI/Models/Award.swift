bd991e3c4e72339f6ee629470426da4e367a846a
Aleksandr F <a7214370@gmail.com>
2022-06-04 23:43:36 +0300

    Work with Award

--- AwardsCollectionAppSwiftUI/Models/Award.swift
+++ AwardsCollectionAppSwiftUI/Models/Award.swift
@@ -5,5 +5,36 @@
 //  Created by Aleksandr F. on 03.06.2022.
 //
 
-import Foundation
+import SwiftUI
 
+
+struct Award {
+    let awardView: AnyView
+    let title: String
+    let awarded: Bool
+    
+    static func getAwards() -> [Award] {
+        [
+            Award(
+                awardView: AnyView(GradientRectangles().frame(width: 160, height: 160)),
+                title: "Gradient Rectangles",
+                awarded: true
+            ),
+            Award(
+                awardView: AnyView(PathView().frame(width: 160, height: 160)),
+                title: "Path View",
+                awarded: true
+            ),
+            Award(
+                awardView: AnyView(CurvesView().frame(width: 160, height: 160)),
+                title: "Curves View",
+                awarded: true
+            ),
+            Award(
+                awardView: AnyView(HypocycloidView(width: 160, height: 160)),
+                title: "Hypocycloid View",
+                awarded: false
+            )
+        ]
+    }
+}
