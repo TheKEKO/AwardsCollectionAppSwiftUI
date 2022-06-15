19bf54c527ef3fe44795ffb5ebf5121958a1c073
Aleksandr F <a7214370@gmail.com>
2022-06-15 16:46:00 +0300

    The app is complete.

--- AwardsCollectionAppSwiftUI/AwardsView.swift
+++ AwardsCollectionAppSwiftUI/AwardsView.swift
@@ -11,18 +11,18 @@ struct AwardsView: View {
     let awards = Award.getAwards().filter { $0.awarded }
     
     var body: some View {
-          NavigationView {
-              CustomGridView(items: awards, columns: 2) { itemSize, award in
-                  VStack {
-                      award.awardView
-                      Text(award.title)
-                  }
-                  .padding()
-                  .frame(width: itemSize, height: itemSize)
-              }
-              .navigationBarTitle("Your awards: \(awards.count)")
-          }
-      }
+        NavigationView {
+            CustomGridView(items: awards, columns: 2) { itemSize, award in
+                VStack {
+                    award.awardView
+                    Text(award.title)
+                }
+                .padding()
+                .frame(width: itemSize, height: itemSize)
+            }
+            .navigationBarTitle("Your awards: \(awards.count)")
+        }
+    }
 }
 
 struct AwardsView_Previews: PreviewProvider {
